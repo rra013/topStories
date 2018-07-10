591ae47ffa394b8b4d6eb74e0e34c0781a09b032
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-10 10:06:11 -0500

    Initial Commit

---
+++ topStories/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  topStories
+//
+//  Created by Rishi Anand on 7/10/18.
+//  Copyright © 2018 Rishi Anand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
