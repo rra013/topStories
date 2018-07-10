108f77e753285a979f8369d234f261d56b1068ee
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-10 11:24:17 -0500

    finished segues

--- topStories/ArticlesViewController.swift
+++ topStories/ArticlesViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class SourcesViewController: UITableViewController {
-    var sources = [[String:String]]()
-    let apiKey = "5d892509a49046a087917c466fa80d09"
+class ArticlessViewController: UITableViewController {
+    var articles = [[String:String]]()
+    var apiKey = ""
+    var source = [String: String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.title = "News Sources"
-        let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
+        self.title = "Top Stories"
+        let query = "https://newsapi.org/v1/articles?" + "source=\(source["id"]!)&apiKey=\(apiKey)"
         DispatchQueue.global(qos: .userInitiated).async {
             [unowned self] in
             if let url = URL(string: query) {
@@ -32,12 +34,12 @@ class SourcesViewController: UITableViewController {
     }
     
     func parse(json: JSON){
-        for result in json["sources"].arrayValue {
-            let id = result["id"].stringValue
-            let name = result["name"].stringValue
+        for result in json["articles"].arrayValue {
+            let title = result["title"].stringValue
             let description = result["description"].stringValue
-            let source = ["id": id, "name": name, "description": description]
-            sources.append(source)
+            let url = result["url"].stringValue
+            let article = ["title": title, "description": description, "url": url]
+            articles.append(article)
         }
         tableView.reloadData()
         DispatchQueue.main.async {
@@ -56,16 +58,16 @@ class SourcesViewController: UITableViewController {
             self.present(alert, animated: true, completion: nil)
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sources.count
+        return articles.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell( withIdentifier: "Cell", for: indexPath)
-        let source = sources[indexPath.row]
-        cell.textLabel?.text = source["name"]
-        cell.detailTextLabel?.text = source["description"]
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article["title"]
+        cell.detailTextLabel?.text = article["description"]
         return cell
     }
     
@@ -73,16 +75,10 @@ class SourcesViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func onTappedDone(_ sender: UIBarButtonItem) {
-        exit(0)
-    }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let dvc = segue.destination as! ArticlessViewController
-        let index = tableView.indexPathForSelectedRow?.row
-        dvc.source = sources[index!]
-        dvc.apiKey = apiKey
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let url = URL(string: articles[indexPath.row]["url"]!)
+        UIApplication.shared.open(url! as URL, options: [:], completionHandler: nil)
     }
 }
 
